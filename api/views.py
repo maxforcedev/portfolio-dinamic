6a44d797b5d59aad75b02e8473a3907e2b720574
Luis Felipe <lfcvs.rj@gmail.com>
2025-07-24 16:56:56 -0300

    feat(api): created views api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import models, serializers
 
-# Create your views here.
+
+class AboutView(generics.ListAPIView):
+    queryset = models.About.objects.all()
+    serializer_class = serializers.AboutSerializer
+
+
+class ProjectListView(generics.ListAPIView):
+    queryset = models.Project.objects.all()
+    serializer_class = serializers.ProjectSerializer
+
+
+class EducationListView(generics.ListAPIView):
+    queryset = models.Education.objects.all()
+    serializer_class = serializers.EducationSerializer
+
+
+class SkillListView(generics.ListAPIView):
+    queryset = models.Skill.objects.all()
+    serializer_class = serializers.SkillSerializer
